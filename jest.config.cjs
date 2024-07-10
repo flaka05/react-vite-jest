0df0f765fc40e33e0638a6bff036d3b3c74c9f7d
amineZRK <mohammedamine1004@gmail.com>
2024-07-10 17:16:23 +0200

    fix: version

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,8 +1,14 @@
 module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'jsdom',
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/src/$1',
-    },
-    setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  };
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy'
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  transform: {
+    '^.+\\.(ts|tsx)$': ['ts-jest', {
+      tsconfig: 'tsconfig.jest.json'
+    }]
+  }
+};
